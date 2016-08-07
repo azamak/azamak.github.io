490cf24f605a5607ebe8f34a6b794041cd444fe6
azamak <azamak@inbox.ru>
2016-08-07 18:14:31 +0300

    corr js auto-slider

--- js/start-slider.js
+++ js/start-slider.js
@@ -1,6 +1,22 @@
-setTimeout(function () {
-    $('#container').ready(function () {
-    $('.controls').click();
+
+
+
+$(Document).ready(function () {
+
+    $('#wel').click(function () {
+    $('#wel').animate({paddingRight:'235px'},2000);
 
 });
+});
+
+
+setTimeout(function () {
+    $('#container').ready(function () {
+        if($('.controls').text()=='Play'){
+                 $('.controls').click();
+        }
+});
+
+
+
 },8000);
