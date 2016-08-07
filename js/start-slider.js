1cde305d667dd725c67fb40d66d2aaf1cd753104
azamak <azamak@inbox.ru>
2016-08-08 01:02:52 +0300

    corr

--- js/start-slider.js
+++ js/start-slider.js
@@ -1,14 +1,5 @@
-$(document).ready(function () {
-
-    $('#wel').click(function () {
-    $('#wel').animate({paddingRight:'235px'},2000);
-
-});
-});
-
-
 setTimeout(function () {
-    $('#container').ready(function () {
+    $('.container').ready(function () {
         var contr=$('.controls');
         if(contr.text()=='Play'){
                  contr.click();
