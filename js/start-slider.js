16fd153af79fb285971099ca9ef5ff692a3dc0d2
azamak <azamak@inbox.ru>
2016-09-12 16:40:39 +0300

    all site
    all site

---
+++ js/start-slider.js
@@ -0,0 +1,11 @@
+setTimeout(function () {
+    $('.container').ready(function () {
+        var contr=$('.controls');
+        if(contr.text()=='Play'){
+                 contr.click();
+        }
+});
+
+
+
+},8000);
