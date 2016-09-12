f85dab6dc5025dbd5293b45dcf1834a9f110519f
azamak <azamak@inbox.ru>
2016-09-12 22:47:54 +0300

    фдд

---
+++ test js/test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by GAMES on 06.08.2016.
+ */
+$(Document).ready(function () {
+   $('.about-img').click(function () {
+       $(".content-inside").animate({marginLeft:"300px"}, 4000)
+
+   })
+});
+
