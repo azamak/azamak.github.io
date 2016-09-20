94d8a0b95ada5f096de1283d370f572d13ff3a70
azamak <azamak@inbox.ru>
2016-09-20 03:09:23 +0300

    all

---
+++ js/menu.js
@@ -0,0 +1,23 @@
+/**
+ * Created by GAMES on 15.09.2016.
+ */
+/*var menu=$('.butt')*/
+$('.menu-all').ready(function () {
+    $('.butt').on('click', function () {
+        var menu = $('.menu-inner li ul li');
+        if (menu.css('display') === 'none') {
+            //menu.css('display','block');
+            menu.fadeIn();
+            menu.css('display', 'block');
+        }
+        else {
+          
+            menu.fadeOut();
+
+        }
+
+
+    })
+
+});
+
