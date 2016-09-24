9a6d37e42a95240a457c9f7f6d9777b517dac9b6
azamak <azamak@inbox.ru>
2016-09-24 09:14:02 +0300

    56

---
+++ js/menuInner.js
@@ -0,0 +1,35 @@
+/**
+ * Created by GAMES on 15.09.2016.
+ */
+/*var menu=$('.butt')*/
+$('.menu-all').ready(function () {
+    $('.butt').on('click', function () {
+
+        var menu = $('.menu-inner ul li');
+        var close = $('.butt');
+        var pict = $('.pictheader');
+
+        if (menu.css('display') === 'none') {
+
+            pict.hide()
+            close.attr( 'src', "../img/menu_close1.png" );
+
+
+            menu.slideDown()
+            menu.css('display','inline-block');
+
+
+        }
+        else {
+            pict.fadeIn(2000)
+
+            menu.slideUp();
+            close.attr( 'src', "../img/menu.png" );
+
+        }
+
+
+    })
+
+});
+
